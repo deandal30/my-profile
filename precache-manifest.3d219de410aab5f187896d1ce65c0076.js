9bc53f5336a2f8060fe4ff9c920b21c73c494ca1
Daniel Eduard Andal <daniel_andal@medilink.com.ph>
2018-11-06 10:03:35 +0800

    Updates

--- precache-manifest.3d219de410aab5f187896d1ce65c0076.js
+++ precache-manifest.3d219de410aab5f187896d1ce65c0076.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/my-profile/static/js/runtime~main.642da487.js"
   },
   {
-    "revision": "0f8cbad4d496b3ae635c",
-    "url": "/my-profile/static/js/main.0f8cbad4.chunk.js"
+    "revision": "ee75d77345d6ba744623",
+    "url": "/my-profile/static/js/main.ee75d773.chunk.js"
   },
   {
     "revision": "cc170037d18c1f0bd831",
     "url": "/my-profile/static/js/1.cc170037.chunk.js"
   },
   {
-    "revision": "0f8cbad4d496b3ae635c",
+    "revision": "ee75d77345d6ba744623",
     "url": "/my-profile/static/css/main.7c8c33ca.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/my-profile/static/css/1.549567ff.chunk.css"
   },
   {
-    "revision": "5ff7c6426643168c4acf964c17566731",
+    "revision": "1240201e7e23920aa9d718b0557d0d59",
     "url": "/my-profile/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-profile/precache-manifest.51987d2787ab250a6da8ccf0246950ff.js"
+  "/my-profile/precache-manifest.3d219de410aab5f187896d1ce65c0076.js"
 );
 
 workbox.clientsClaim();
